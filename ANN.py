641a0192a815fdf59e0776a2da48c1c4bd5e42ba
David Zhao <davidzhao058@gmail.com>
2017-09-17 04:52:21 -0400

    Clean ann.py

--- ANN.py
+++ ANN.py
@@ -4,33 +4,30 @@ import pickle
 
 class BasicNeuralNetwork():
 	def __init__(self, filepath=None):
-		self.dim = [5, 4, 2]
-		self.L = len(self.dim)
-		self.theta = []
-		self.lam = 0.05
-		self.learning_rate = 0.01
-		self.filepath = filepath
+		self.dim = [5, 4, 2]		# layer dimensions
+		self.L = len(self.dim)		# no. of layers
+		self.theta = []				# weights
+		self.lambda_reg = 0.05	    # regularization factor
+		self.learning_rate = 0.01   # learning factor
+		self.filepath = filepath	# file path to save weights
+
 		self.initWeights()
 		self.sigmoid = np.vectorize(self.sigmoid)
 
-	# Initialize random weights for each layer
-	# An additional bias node is prepended to the first layer in each pair of layers
-	# 	If you had layers of dimensions 4, 2, and 1, the weights would be initialized
-	#	as arrays of dimensions as such, [[2x5],[1x3]].
 	def initWeights(self):
-		# if self.filepath == None or not os.path.isfile(self.filepath):
-		for cnt in range(self.L-1):
-			self.theta.append((np.random.rand(self.dim[cnt+1], self.dim[cnt]+1)-0.5)*5)
-		# else:
-			# self.theta = pickle.load(open(self.filepath, 'rb'))
+		"""Initialize weights for each layer."""
+		if self.filepath is None or not os.path.isfile(self.filepath):
+			for cnt in range(self.L-1):
+				self.theta.append((np.random.rand(self.dim[cnt+1], self.dim[cnt]+1)-0.5)*5)
+		else:
+			self.theta = pickle.load(open(self.filepath, 'rb'))
 
-	# Apply a sigmoid function to an input z,
-	# vectorized to work on numpy structures
 	def sigmoid(self, z):
+		"""Apply sigmoid function."""
 		return 1/(1+np.exp(-z))
 
-	# Forward propagate with x, an input point
 	def forwardPropOne(self, x):
+		"""Return the values of each post-activation layer."""
 		x = np.array(x)
 		a = [x]
 		for cnt in range(self.L-1):
@@ -39,29 +36,31 @@ class BasicNeuralNetwork():
 		return a
 
 	def predict(self, x):
+		"""Return prediction for a single input vector."""
 		x = np.array(x)
-		for cnt in range(self.L - 1):
+		for cnt in range(self.L-1):
 			x = self.sigmoid(np.dot(self.theta[cnt], np.insert(x, 0, 1, axis=0)))
 		return np.argmax(x)
 
-	# Backward propagate to update weights given
-	# the forward propagation activation values, a,
-	# and the expected result, y
 	def backwardProp(self, a, y):
+		"""Return weight update gradient."""
 		y = np.array(y)
 		sens = [a[-1]-y] # sensitivities of the cost function to each pre-activation value
 						# initialized with the last layer sensitivity of output - expected
+		# Calculate sensitivities for each layer
 		for cnt in range(self.L-2, 0, -1):
 			sens.insert(0, np.multiply(np.dot(self.theta[cnt].T[1:],sens[0]), np.multiply(a[cnt],1-a[cnt])))
+		# Calculate weight update gradients for each layer
 		grad = []
 		for cnt in range(self.L-1):
 			grad.append(np.dot(sens[cnt][:,None], np.insert(a[cnt],0,1,axis=0)[:,None].T))
 		return grad
 
 	def trainOnData(self, filepath):
-		print('Training on data')
+		"""Train neural network using data at filepath."""
 		x = []
 		y_tmp = []
+		# Parse data at filepath into training arrays
 		if os.path.isfile(filepath):
 			with open(filepath, 'r') as f:
 				for line in f.readlines():
@@ -71,37 +70,30 @@ class BasicNeuralNetwork():
 		y = np.zeros((len(y_tmp), self.dim[-1]))
 		y[np.arange(len(y_tmp)), y_tmp] = 1
 		y = y.tolist()
+		# Shuffle training data
 		zipped = np.array([i for i in zip(x,y)])
-		# print(zipped.tolist())
 		np.random.shuffle(zipped)
-		# print(zipped.tolist())
 		x_rand = []
 		y_rand = []
 		for _x, _y in zipped.tolist():
 			x_rand.append(_x)
 			y_rand.append(_y)
-		print(len(x_rand), len(y_rand))
-		print([len(i) for i in x_rand])
-		print([len(i) for i in y_rand])
+		# Train using parsed data
 		self.train(x_rand, y_rand)
 
 	def train(self, x, y):
-		print('Train')
+		"""Train with input and expected data arrays."""
 		grad = [0*t for t in self.theta]
 		reg = self.theta
 		m = len(y)
 		for n in range(20):
-			print('Epoch',n)
 			for i in range(m):
-				# a = forwardPropOne([[x[0][i]], [x[1][i]]])
 				a = self.forwardPropOne(x[i])
-				# print('a', a)
 				g = self.backwardProp(a,y[i])
 				grad = [grad[k] + g[k] for k in range(self.L-1)]
 			for i in range(self.L-1):
 				reg[i].T[0] = 0
-				self.theta[i] = self.theta[i] - self.learning_rate*(grad[i]/m + self.lam*reg[i])
-				# print(forwardPropOne(x))
+				self.theta[i] = self.theta[i] - self.learning_rate*(grad[i]/m + self.lambda_reg*reg[i])
 		if self.filepath != None:
 			pickle.dump(self.theta, open(self.filepath, 'wb'))
 		return
